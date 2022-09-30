2c20e05b51bb3f7f8ede9f7384080678317ccea2
Victor Gila Barreto <gilabarreto@gmail.com>
2022-09-30 14:22:39 -0600

    Implement validation for Save button and test.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("")
@@ -20,6 +21,17 @@ export default function Form(props) {
     reset()
   };
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,8 +44,10 @@ export default function Form(props) {
             value={student}
             placeholder="Enter Student Name"
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           value={interviewer}
           interviewers={interviewers}
@@ -43,7 +57,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
